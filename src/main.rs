33797de1b3391274945a0e97ba851c55b6c47637
lavazazazavr <lavazazazavr@gmail.com>
2025-10-28 21:53:06 +0100

    выыым

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,39 @@
-fn main() {
+use std::io;
 
+fn main() {
+    let mut sum = 0;
+
+    loop {
+        let mut input = String::new();
+        match io::stdin().read_line(&mut input) {
+            Ok(_) => {}
+            Err(_) => {
+                println!("NaN");
+                return;
+            }
+        }
+
+        let input = input.trim();
+
+        if input == "-1" {
+            break;
+        }
+
+        match input.parse::<i32>() {
+            Ok(num) => {
+                if num > 0 {
+                    sum += num;
+                } else {
+                    println!("NaN");
+                    return;
+                }
+            }
+            Err(_) => {
+                println!("NaN");
+                return;
+            }
+        }
+    }
+
+    println!("{}", sum);
 }
